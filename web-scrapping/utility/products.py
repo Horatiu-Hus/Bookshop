e38d537db33e6e41a1e704437568abc8ebfd859a
Horatiu-Hus <112974677+Horatiu-Hus@users.noreply.github.com>
2022-11-09 19:31:16 +0200

    Web Scrapper

--- web-scrapping/utility/products.py
+++ web-scrapping/utility/products.py
@@ -8,7 +8,7 @@ from utility.base import BaseElement
 class Product(BaseElement):
     def __init__(self, element, driver):
         super().__init__(element, driver)
-        self._id = uuid4()
+        self._title = None
         self._price = None
         self._image_path = None
         self._specifications = {}
@@ -19,43 +19,42 @@ class Product(BaseElement):
         self._driver.execute_script(f'window.open("{link_element.get_attribute("href")}")')
         self._driver.switch_to.window(self._driver.window_handles[-1])
 
-        self._title = self._driver.find_element(By.XPATH, './/h1[@class="page-title"]').text
-
-        self._price = float(
-            self._driver.find_element(
-                By.XPATH, './/div[contains(@class, "product-main-area")]//p[contains(@class, "product-new-price")]'
-            ).text.replace(' Lei', '').replace('.', '').replace(',', '.')
-        )
-
-        image_element = self._driver.find_element(By.XPATH, './/div[@data-ph-id="image-0"]//img')
-        image_data = requests.get(url=image_element.get_attribute('src')).content
-        self._image_path = f'./images/{self._id}.jpg', 'wb'
-        with open(f'./images/{self._id}.jpg', 'wb') as image_file:
-            image_file.write(image_data)
-
-        button = self._driver.find_element(By.XPATH, '//button[@data-ph-target="#specifications-body"]')
-        button.click()
-
-        sleep(0.5)
-
-        specifications_body = self._driver.find_element(By.ID, 'specifications-body')
-        title_elements = specifications_body.find_elements(By.TAG_NAME, 'p')
-        table_elements = specifications_body.find_elements(By.TAG_NAME, 'table')
-
-        for title_element, table_element in zip(title_elements, table_elements):
-            specs_title = title_element.text
-            self._specifications[specs_title] = {}
-
-            row_elements = table_element.find_elements(By.TAG_NAME, 'tr')
-            for row_element in row_elements:
-                cells = row_element.find_elements(By.TAG_NAME, 'td')
-                subcategory = cells[0].text
-                value = cells[1].text
-
-                self._specifications[specs_title][subcategory] = value
-
         try:
-            pass
+            self._title = self._driver.find_element(By.XPATH, './/h1[@class="page-title"]').text
+
+            self._price = float(
+                self._driver.find_element(
+                    By.XPATH, './/div[contains(@class, "product-main-area")]//p[contains(@class, "product-new-price")]'
+                ).text.replace(' Lei', '').replace('.', '').replace(',', '.')
+            )
+
+            image_element = self._driver.find_element(By.XPATH, './/div[@data-ph-id="image-0"]//img')
+            image_data = requests.get(url=image_element.get_attribute('src')).content
+            self._image_path = f'./images/{self._title}.jpg', 'wb'
+            with open(f'./images/{self._title}.jpg', 'wb') as image_file:
+                image_file.write(image_data)
+
+            button = self._driver.find_element(By.XPATH, '//button[@data-ph-target="#specifications-body"]')
+            button.click()
+
+            sleep(0.5)
+
+            specifications_body = self._driver.find_element(By.ID, 'specifications-body')
+            title_elements = specifications_body.find_elements(By.TAG_NAME, 'p')
+            table_elements = specifications_body.find_elements(By.TAG_NAME, 'table')
+
+            for title_element, table_element in zip(title_elements, table_elements):
+                specs_title = title_element.text
+                self._specifications[specs_title] = {}
+
+                row_elements = table_element.find_elements(By.TAG_NAME, 'tr')
+                for row_element in row_elements:
+                    cells = row_element.find_elements(By.TAG_NAME, 'td')
+                    subcategory = cells[0].text
+                    value = cells[1].text
+
+                    self._specifications[specs_title][subcategory] = value
+
         except Exception as e:
             print(e)
 
@@ -64,7 +63,7 @@ class Product(BaseElement):
 
     def to_dict(self):
         return {
-            'id': self._id,
+            'title': self._title,
             'price': self._price,
             'image_path': self._image_path,
             'specifications': self._specifications
